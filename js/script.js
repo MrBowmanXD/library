a68fb698de31f3a9c68213df7c27a33088de7ef7
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2022-03-12 18:12:13 +0000

    Functional button to display form

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
+const addBook = document.querySelector('.addBook');
+const bookForm = document.querySelector('.bookForm');
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  };
 }
+
+Book.prototype.info = function () {
+  return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+};
+
+function addBookToLibrary() {}
 
 const harryPotter = new Book('Harry Potter', 'Woman', 698, 'not read yet');
 
-console.log(harryPotter.info());
+addBook.addEventListener('click', () => {
+  bookForm.classList.toggle('hidden');
+});
