4ba5342438c4530a0295b87967a0f0ddc43b7aae
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2022-03-13 00:31:49 +0000

    Version 0.9

--- js/script.js
+++ js/script.js
@@ -7,12 +7,14 @@ const pages = document.querySelector('#pages');
 const read = document.querySelector('#read');
 const flexContainer = document.querySelector('.flex-container');
 let myLibrary = [];
+let counter = 1;
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, counter) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.counter = counter;
 }
 
 Book.prototype.info = function () {
@@ -28,11 +30,28 @@ addBook.addEventListener('click', () => {
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  let newBook = new Book(title.value, author.value, pages.value, read.value);
+  let newBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    read.value,
+    counter++
+  );
   myLibrary.push(newBook);
 
   loopBooks();
   formToggler();
+  myLibrary.forEach((book) => {
+    const selectedBook = document.querySelector(`.counter${book.counter}`);
+    const removedBook = document.querySelector(`.counter${book.counter}Remove`);
+    selectedBook.addEventListener('click', () => {
+      removedBook.classList.add('hidden');
+      book.removed = true;
+    });
+    if (book.removed) {
+      flexContainer.removeChild(removedBook);
+    }
+  });
 });
 
 function formToggler() {
@@ -52,10 +71,12 @@ function loopBooks() {
   myLibrary.forEach((book) => {
     const div = document.createElement('div');
     div.classList.add('flex-child');
+    div.classList.add(`counter${book.counter}Remove`);
     div.innerHTML += `<p class="bold">Title</p><p>${book.title}</p>
   <p class="bold">Author</p><p>${book.author}</p>
   <p class="bold">Pages</p><p>${parseInt(book.pages)}</p>
-  <p class="bold">Read</p><p>${book.read}</p>`;
+  <p class="bold">Read</p><p>${book.read}</p>
+  <button class="remove counter${book.counter}">Remove</button>`;
 
     flexContainer.appendChild(div);
   });
