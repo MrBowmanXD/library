8e3282b3ce3c98adf3a503673a480120d1205f09
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2022-03-16 11:41:07 +0000

    Added classes to code

--- js/script.js
+++ js/script.js
@@ -6,43 +6,90 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const read = document.querySelector('#read');
 const flexContainer = document.querySelector('.flex-container');
-let myLibrary = [];
-let counter = 1;
+myLibrary = [];
 
-function Book(title, author, pages, read, counter) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.counter = counter;
+class Book {
+  myLibrary = [];
+  title = 'unknown';
+  author = 'unknown';
+  pages = null;
+  read = 'not read yet';
+  counter = 1;
+
+  constructor(title, author, pages, read, counter) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.counter = counter;
+  }
+
+  info() {
+    return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  }
+
+  loopBooks() {
+    myLibrary.forEach((book) => {
+      const div = document.createElement('div');
+      div.classList.add('flex-child');
+      div.classList.add(`counter${book.counter}Remove`);
+      div.innerHTML += `<p class="bold">Title</p><p>${book.title}</p>
+    <p class="bold">Author</p><p>${book.author}</p>
+    <p class="bold">Pages</p><p>${parseInt(book.pages)}</p>
+    <p class="bold readMaybe">Read</p><input class="checkbox${
+      book.counter
+    }" type="checkbox"><p class="userRead${book.counter}">${book.read}</p>
+    <button class="remove counter${book.counter}">Remove</button>`;
+
+      flexContainer.appendChild(div);
+    });
+  }
+
+  removeBook() {
+    myLibrary.forEach((book) => {
+      const selectedBook = document.querySelector(`.counter${book.counter}`);
+      const removedBook = document.querySelector(
+        `.counter${book.counter}Remove`
+      );
+      selectedBook.addEventListener('click', () => {
+        removedBook.classList.add('hidden');
+        book.removed = true;
+      });
+      if (book.removed) {
+        flexContainer.removeChild(removedBook);
+      }
+    });
+  }
+
+  addCheckBoxChecker() {
+    myLibrary.forEach((book) => {
+      const checkbox = document.querySelector(`.checkbox${book.counter}`);
+      const userRead = document.querySelector(`.userRead${book.counter}`);
+      checkbox.addEventListener('change', (e) => {
+        if (e.target.checked) {
+          userRead.textContent = 'Yes i read it';
+        } else {
+          userRead.textContent = 'Not read yet';
+        }
+      });
+    });
+  }
 }
 
-Book.prototype.info = function () {
-  return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-};
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let newBook = new Book(title.value, author.value, pages.value, read.value);
+  myLibrary.push(newBook);
 
-// const harryPotter = new Book('Harry Potter', 'Woman', 698, 'not read yet');
+  newBook.loopBooks();
+  formToggler();
+  newBook.addCheckBoxChecker();
+  newBook.removeBook();
+});
 
 addBook.addEventListener('click', () => {
   formToggler();
   removeFlexChild();
-});
-
-submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  let newBook = new Book(
-    title.value,
-    author.value,
-    pages.value,
-    read.value,
-    counter++
-  );
-  myLibrary.push(newBook);
-
-  loopBooks();
-  formToggler();
-  addCheckBoxChecker();
-  removeBook();
 });
 
 function formToggler() {
@@ -55,50 +102,5 @@ function removeFlexChild() {
 
   flexChildrenArray.forEach((child) => {
     flexContainer.removeChild(child);
-  });
-}
-
-function loopBooks() {
-  myLibrary.forEach((book) => {
-    const div = document.createElement('div');
-    div.classList.add('flex-child');
-    div.classList.add(`counter${book.counter}Remove`);
-    div.innerHTML += `<p class="bold">Title</p><p>${book.title}</p>
-  <p class="bold">Author</p><p>${book.author}</p>
-  <p class="bold">Pages</p><p>${parseInt(book.pages)}</p>
-  <p class="bold readMaybe">Read</p><input class="checkbox${
-    book.counter
-  }" type="checkbox"><p class="userRead${book.counter}">${book.read}</p>
-  <button class="remove counter${book.counter}">Remove</button>`;
-
-    flexContainer.appendChild(div);
-  });
-}
-
-function removeBook() {
-  myLibrary.forEach((book) => {
-    const selectedBook = document.querySelector(`.counter${book.counter}`);
-    const removedBook = document.querySelector(`.counter${book.counter}Remove`);
-    selectedBook.addEventListener('click', () => {
-      removedBook.classList.add('hidden');
-      book.removed = true;
-    });
-    if (book.removed) {
-      flexContainer.removeChild(removedBook);
-    }
-  });
-}
-
-function addCheckBoxChecker() {
-  myLibrary.forEach((book) => {
-    const checkbox = document.querySelector(`.checkbox${book.counter}`);
-    const userRead = document.querySelector(`.userRead${book.counter}`);
-    checkbox.addEventListener('change', (e) => {
-      if (e.target.checked) {
-        userRead.textContent = 'Yes i read it';
-      } else {
-        userRead.textContent = 'Not read yet';
-      }
-    });
   });
 }
